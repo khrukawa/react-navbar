7f7e37119d2b8f5c641a4c8567027c98766c05d8
khrukawa <khrukawa@gmail.com>
2020-05-07 11:47:38 +0630

    initial commit;

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import './App.css';
+import Home from './components/index';
+
+
+
+function App() {
+  return (
+    <>
+      <CssBaseline />
+      <Route exact path='/' component={Home} />
+    </>
+  );
+}
+
+export default App;
